1e4b8dac07a63b69469c5068e8df7c1f67e97274
vritta <anuhartripathi15@gmail.com>
2024-10-06 21:45:12 +0530

    added model_dump() method and pydantic validator mode

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
     return {"message":"Hi cutie !"}
 
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    return {"title":f"{payload["post_title"]}", "content": f"{payload["post_content"]}"}
+def create_post(new_post: Post):
+    print(new_post)
+    return new_post.model_dump()
+    # return {"title":f"{new_post["title"]}", "content": f"{new_post["content"]}"}
