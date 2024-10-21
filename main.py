49941daa66e72dbd48479729f0b53a924edd88d0
vritta <anuhartripathi15@gmail.com>
2024-10-21 22:50:37 +0530

    Insert data in variable through post

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
@@ -5,17 +6,23 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+my_data = [{"title": "abc", "content": "cont1", "rating": 1, "id": 0}]
 class Post(BaseModel):
+    id: int
     title: str
     content: str
     rating: Optional[int] = None
 
+my_post = [{}]
 @app.get("/")
 async def root():
     return {"message":"Hi cutie !"}
 
 @app.post("/posts")
 def create_post(new_post: Post):
+    new_post = new_post.model_dump()
+    new_post["id"] = randint(0,100)
     print(new_post)
-    return new_post.model_dump()
-    # return {"title":f"{new_post["title"]}", "content": f"{new_post["content"]}"}
+    my_data.append(new_post)
+    # my_data.model_dump()
+    return {"data":my_data}
