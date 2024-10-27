a8f51027b4ad569762d9061a6f0f21712abf1d26
vritta <anuhartripathi15@gmail.com>
2024-10-27 13:01:34 +0530

    Added status code in api result

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from random import randint
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
 
 my_data = [{"title": "abc", "content": "cont1", "rating": 1, "id": 0}]
+
+def find_post(id):
+    for p in my_data:
+        if p["id"]==id:
+            return p
+
 class Post(BaseModel):
     id: int
     title: str
@@ -26,3 +32,12 @@ def create_post(new_post: Post):
     my_data.append(new_post)
     # my_data.model_dump()
     return {"data":my_data}
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} not found")
+    # return post
+    return {"post_detail":post}
